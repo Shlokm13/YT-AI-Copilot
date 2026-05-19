50d1935de7ec64f41870354ffbae31f9d82e7e61
Shlok Mishra <shlokm2065@gmail.com>
2026-05-19 22:19:13 +0530

    Allow iframe embedding

--- ytChatBot/frontend/next.config.mjs
+++ ytChatBot/frontend/next.config.mjs
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *;",
+          },
+        ],
+      },
+    ]
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
